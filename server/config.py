5cab5eef6e447c25d3f8db432c62cf83e8fbc04f
Spencer <eklund.spencer@outlook.com>
2024-02-23 08:17:10 -0700

    trying a new solution

--- server/config.py
+++ server/config.py
@@ -5,9 +5,10 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
